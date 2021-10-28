29f9f0e934d5946497b09c9dbec630b21b9c02d3
cemre su ant <cemresuant@gmail.com>
2021-10-28 23:12:37 +0300

    18.Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,29 @@
+console.log("Mehaba Kodlama.io")
+
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+dolarDun = "9.20"
+//  {
+//      var dolarDun = 9.10
+//  }
+
+ console.log(dolarDun)
+
+ const euroDun = 11.2
+ //euroDun = 11 ( BUNU YAPMA !!)
+ console.log(euroDun)
+
+
+ //array
+ //let users =getUsersFromApi
+ let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"," ÖzelKonut Kredisi"]
+
+ console.log("<ul>")
+ for(let i = 0;i<konutKredileri.length;i++){
+     console.log("<li>"+konutKredileri[i]+"</li>")
+ }
+ console.log("</ul>")
+
+//  console.log(konutKredileri)
+ 
